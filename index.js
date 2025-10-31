b907a980201fb54575c5e7f2ce14367b14e884ec
Abhishek Kumar Yadav <yadavabhishek5869@gmail.com>
2025-10-31 09:50:20 +0530

    added user

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/user", (req, res) => {
+  res.send({
+    user_name: "Abhishek Kumar",
+    user_id: "539eba02-f786-412c-91d5-620769371395",
+  });
+});
+
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${process.env.PORT}`);
 });
